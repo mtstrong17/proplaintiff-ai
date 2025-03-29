762b158bab2e741286787573d0fa76d0c53ec12a
Michael Strong <michael@fyresite.com>
2025-03-29 10:32:07 -0700

    add prettier

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,7 @@ import eslintConfigPrettier from "eslint-config-prettier"
 import onlyWarn from "eslint-plugin-only-warn"
 import turboPlugin from "eslint-plugin-turbo"
 import tseslint from "typescript-eslint"
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /**
  * A shared ESLint configuration for the repository.
@@ -13,6 +14,7 @@ export const config = [
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
     plugins: {
       turbo: turboPlugin,
