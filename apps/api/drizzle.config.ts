9217c1d33c237b185a6db43e34b5a01de6739d58
Michael Strong <michael@fyresite.com>
2025-04-04 11:06:35 -0700

    update rules

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -6,9 +6,9 @@ dotenv.config();
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL || '',
   },
   verbose: true,
   strict: true,
