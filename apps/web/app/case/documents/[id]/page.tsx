b60b2998a60aba9a8f90d67ffe4bbdf56fd25ec7
Michael Strong <michael@fyresite.com>
2025-04-05 11:19:15 -0700

    fixed errors

--- apps/web/app/case/documents/[id]/page.tsx
+++ apps/web/app/case/documents/[id]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import React from 'react';
 import { DocumentDetails } from '../components/document-details';
 
-export default function DocumentPage({ params }: { params: { id: string } }) {
+export default function DocumentPage({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = React.use(params);
   return (
     <div className="container mx-auto py-8 px-4">
-      <DocumentDetails documentId={params.id} />
+      <DocumentDetails documentId={resolvedParams.id} />
     </div>
   );
 }
